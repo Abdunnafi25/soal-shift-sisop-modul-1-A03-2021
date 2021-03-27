cd44ca1d7c3e7fa998ae0cbc404f9b8af3e56e48
Rizarize <81413071+Rizarize@users.noreply.github.com>
2021-03-27 17:48:09 +0700

    Delete no3 directory

--- no3/3b.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tgl=$(date +"%d-%m-%Y")
-mkdir "$tgl"
-bash 3a.sh
-mv Koleksi_*.jpg Foto.log $tgl
-
-
-
--- no3/3d.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pass=$(date +"%d%m%Y")
-zip -P "$pass" -rm Koleksi.zip Kucing_* Kelinci_*
